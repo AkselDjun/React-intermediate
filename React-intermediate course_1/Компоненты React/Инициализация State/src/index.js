a94306a3251e6007653477d93ad5c21c5188086b
AkselDjun <akseldjun@gmail.com>
2020-06-07 21:55:16 +0300

    React component step_!

---
+++ React-intermediate course_1/Компоненты React/Инициализация State/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App title={"I am from props!"} />, document.getElementById('root'));
+serviceWorker.unregister();
